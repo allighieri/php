4ed9cd8db50c39af35329605e0b6924ac47f9f0f
Weder Monteiro <agenciaolhardigital@gmail.com>
2020-06-13 14:38:40 -0300

    update com DAO

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -49,16 +49,99 @@ class Usuario{
 
         if(count($results) > 0){
 
-            $row = $results[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
             
         }
 
     }
+
+    //o fato desse método ser estatico é que ele não precisará ser instanciado ao ser chamado
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+    }
+
+    //o fato desse método ser estatico é que ele não precisará ser instanciado ao ser chamado
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ":SEARCH"=>"%".$login."%"
+        ));
+
+    }    
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if(count($results) > 0){
+
+            $this->setData($results[0]);
+            
+        } else{
+
+            throw new Exception("Login e/ou senha inválidos");
+
+        }
+
+    }
+
+    public function setData($data){
+
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));  
+
+    }
+
+    public function insert(){
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha()
+        ));
+
+        if(count($results) > 0){
+            $this->setData($results[0]);
+        }
+
+    }
+
+    public function update($login, $password){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha(),
+            ":ID"=>$this->getIdusuario()
+        ));
+
+    }
+
+    public function __construct($login = "", $password = ""){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);        
+
+    }
+
 
     public function __toString(){
 
--- DAO/index.php
+++ DAO/index.php
@@ -2,14 +2,38 @@
 
     require_once("config.php");
 
-    $root = new Usuario();
+    //tras um só usuário de acordo com o id pesquisado
+    // $root = new Usuario();
+    //$root->loadById(2);
+    //echo $root;
 
-    $root->loadById(2);
+    //Carrega uma lista de usuario
+    //$lista = Usuario::getList();
+    //echo json_encode($lista);
 
-    echo $root;
+    //Carrega uma lista de usuários buscando pelo login
+    //$search = Usuario::search("we");
+    //echo json_encode($search);
 
+    //carrega um usuario usando o login e a senha
+    //$usuario = new Usuario();
+    //$usuario->login("user","12345");
 
+    //echo $usuario;
 
+    //INSERIR COM PROCEDURE
+    //$aluno = new Usuario("Weder", "12345698");
+    //$aluno->insert();
+    //echo $aluno;
+
+    //Updade registros
+    $usuario = new Usuario();
+
+    $usuario->loadById(13);
+
+    $usuario->update("Sullivan", "agencia5859262");
+
+    
 
 
 /*    $sql = new Sql();
